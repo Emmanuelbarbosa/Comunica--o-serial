47ca45af932585bf589316df1f39825de69aabe2
Emmanuel Barbosa <emmanuebs22@gmail.com>
2014-06-28 08:58:05 -0300

    Código em C++ para a comunicação

---
+++ Comunicação_Serial/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+#include "comunicacao.h"
+//#include <cstdlib>
+//#include <string>
+
+
+using namespace std;
+
+int main (){
+Serial Porta("COM5");
+char bytes_pra_envio[] = "Emmanuel";
+
+Porta.Write(bytes_pra_envio);
+
+//Porta.Read();
+Porta.Close();
+
+
+	system("pause\n\n");
+}
+
